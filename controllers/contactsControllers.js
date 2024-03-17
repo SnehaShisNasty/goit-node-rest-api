4802d09d936856c5c3f600aad61b96f4afd9b746
SnehaShisNasty <katirina18@gmail.com>
2024-03-17 19:40:28 +0000

    commit

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,78 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  getAll,
+  get,
+  create,
+  update,
+  deleteCon,
+} from "../services/contactsServices.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+import HttpError from "../helpers/HttpError.js";
+export const getAllContacts = async (req, res) => {
+  try {
+    const result = await getAll();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getAllContacts = (req, res) => {};
+// export const getOneContact = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const result = await get(id);
+//     if (!result) {
+//       throw HttpError(404, `Movie with id=${id} not found`);
+//     }
+//     res.json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export const getOneContact = (req, res) => {};
+// export const deleteContact = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const result = await deleteCon(id);
+//     if (!result) {
+//       throw HttpError(404, `Movie with id=${id} not found`);
+//     }
+//     res.json({
+//       message: "Delete success",
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export const deleteContact = (req, res) => {};
+// export const createContact = async (req, res) => {
+//   try {
+//     const { error } = createContactSchema.validate(req.body);
+//     if (error) {
+//       throw HttpError(400, error.message);
+//     }
+//     const result = await create(req.body);
+//     res.status(201).json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export const createContact = (req, res) => {};
-
-export const updateContact = (req, res) => {};
+// export const updateContact = async (req, res) => {
+//   try {
+//     const { error } = updateContactSchema.validate(req.body);
+//     if (error) {
+//       throw HttpError(400, error.message);
+//     }
+//     const { id } = req.params;
+//     const result = await update(id, req.body);
+//     if (!result) {
+//       throw HttpError(404, `Movie with id=${id} not found`);
+//     }
+//     res.json(result);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
---
+++ services/contactsServices.js
@@ -0,0 +1,48 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+const contactsPath = path.resolve("dp", "contacts.json");
+export const getAll = async () => {
+  const data = await fs.readFile(contactsPath);
+
+  return JSON.parse(data);
+};
+export const get = async (id) => {
+  const data = await getAll();
+  const result = data.find((item) => item.id === id);
+
+  return result || null;
+};
+export const create = async (data) => {
+  const contacts = await getAll();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+export const update = async (id, data) => {
+  const contacts = await getAll();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { ...contacts[index], ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return contacts[index];
+};
+export const deleteCon = async (id) => {
+  const contacts = await getAll();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
